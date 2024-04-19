1a784447b5ca3acd9f3a4688a64d20966980e4bc
aqasemi <alqasemy2020@gmail.com>
2024-04-19 02:48:31 +0000

    chg from_file method

--- auth/providers/google.py
+++ auth/providers/google.py
@@ -8,16 +8,9 @@ import os, sys
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev | remove in production 
 os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"]  = "1"
 
-try:
-    client_secret_config = loads(os.environ["GOOGLE_CLIENT_SECRET"])
-    flow = Flow.from_client_config(
-        client_config=client_secret_config,
-        scopes="openid profile email"
-    )
-except:
-    client_secrets_file = os.getcwd() +"/"+ os.environ["GOOGLE_CLIENT_SECRET"]
+client_secrets_file = os.getcwd() +"/"+ os.environ["GOOGLE_CLIENT_SECRET"]
 
-    flow = Flow.from_client_secrets_file(
-        client_secrets_file=client_secrets_file,
-        scopes="openid profile email"
-    )
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes="openid profile email"
+)
